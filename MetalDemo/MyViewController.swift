ba306f23f3f803ba85c08c4bd320ddace9513bac
visla-zhupeiran <peiran.zhu@visla.us>
2021-03-26 22:53:29 +0800

    init project.

---
+++ MetalDemo/MyViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MyViewController.swift
+//  MetalDemo
+//
+//  Created by VislaNiap on 2021/3/26.
+//
+
+import UIKit
+import MetalKit
+
+class MyViewController:UIViewController{
+    
+    var metalView:MTKView{
+        return view as! MTKView
+    }
+    
+    override func viewDidLoad() {
+        
+    }
+}
